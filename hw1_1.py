8348232741307e917bf259fbb7f02a9fcd5d5f12
Andrew-Rom <1811mail@gmail.com>
2023-08-26 15:31:39 +0300

    HW1_1 Completed

--- hw1_1.py
+++ hw1_1.py
@@ -1,29 +1,33 @@
-MULTIPLIER_START_NUM = 2
-MULTIPLIER_END_NUM = 11
-TABLE_START_NUM = 2
-TABLE_END_NUM = 10
-NUMBER_OF_COLUMNS = 4
+multiplier_start_num = 2
+multiplier_end_num = 10
+table_start_num = 2
+table_end_num = 9
+number_of_columns = 4
 
-# print('ТАБЛИЦА УМНОЖЕНИЯ')
-#
-for multiplier in range (MULTIPLIER_START_NUM, MULTIPLIER_END_NUM):
-    for counter in range(NUMBER_OF_COLUMNS):
-        for number in range (TABLE_START_NUM, TABLE_END_NUM):
+print('ТАБЛИЦА УМНОЖЕНИЯ'.center(60))
+
+for multiplier in range (multiplier_start_num, multiplier_end_num + 1):
+    counter = 0
+    for number in range ((table_start_num), table_end_num + 1):
+        if counter < number_of_columns:
             print(f'{number} x {multiplier:<2} = {number * multiplier:>2}', end='\t\t')
-
+            counter += 1
+        else:
+            print()
+            break
 
 print()
-#
-# for multiplier in range (MULTIPLIER_START_NUM, MULTIPLIER_END_NUM):
-#     counter = 0
-#     for number in range ((TABLE_START_NUM + NUMBER_OF_COLUMNS), TABLE_END_NUM):
-#         if counter < NUMBER_OF_COLUMNS:
-#             print(f'{number} x {multiplier:<2} = {number * multiplier:>2}', end='\t\t')
-#             counter += 1
-#         else:
-#             print()
-#             break
-#     print()
+
+for multiplier in range (multiplier_start_num, multiplier_end_num + 1):
+    counter = 0
+    for number in range ((table_start_num + number_of_columns), table_end_num + 1):
+        if counter < number_of_columns:
+            print(f'{number} x {multiplier:<2} = {number * multiplier:>2}', end='\t\t')
+            counter += 1
+        else:
+            print()
+            break
+    print()
 
 
 
