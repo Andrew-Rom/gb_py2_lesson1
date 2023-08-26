2b94bb16e6da8f65d1a90e7214522cd9c93be3f6
Andrew-Rom <1811mail@gmail.com>
2023-08-26 15:03:34 +0300

    HW1_1 Create two row table

--- hw1_1.py
+++ hw1_1.py
@@ -3,9 +3,27 @@ MULTIPLIER_END_NUM = 11
 TABLE_START_NUM = 2
 TABLE_END_NUM = 10
 NUMBER_OF_COLUMNS = 4
+
+# print('ТАБЛИЦА УМНОЖЕНИЯ')
+#
 for multiplier in range (MULTIPLIER_START_NUM, MULTIPLIER_END_NUM):
-    for number in range (TABLE_START_NUM, TABLE_END_NUM):
-        print(f'{number} x {multiplier:<2} = {number * multiplier:>2}', end='\t\t')
-    print()
+    for counter in range(NUMBER_OF_COLUMNS):
+        for number in range (TABLE_START_NUM, TABLE_END_NUM):
+            print(f'{number} x {multiplier:<2} = {number * multiplier:>2}', end='\t\t')
+
+
+print()
+#
+# for multiplier in range (MULTIPLIER_START_NUM, MULTIPLIER_END_NUM):
+#     counter = 0
+#     for number in range ((TABLE_START_NUM + NUMBER_OF_COLUMNS), TABLE_END_NUM):
+#         if counter < NUMBER_OF_COLUMNS:
+#             print(f'{number} x {multiplier:<2} = {number * multiplier:>2}', end='\t\t')
+#             counter += 1
+#         else:
+#             print()
+#             break
+#     print()
+
 
 
